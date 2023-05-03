352eb7d451028e8f56b79e864572cc2bd5a1c0a7
Mahmoud Magdy <mahmoudmagdy.r7@gmail.com>
2023-05-04 01:11:31 +0300

    update render error

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -1,63 +1,62 @@
 import React from "react";
-import logo1 from '../assets/images/placeholder.png'
 export default function BookDetails() {
-  return <>
+  return (
+    <>
       <div className="pt-20 max-w-screen-xl mx-auto grid grid-cols-6 lg:col-span-5 gap-2">
+        <aside className="  col-span-4 md:col-span-2 md:col-start-3 lg:col-span-1 col-start-2">
+          <img
+            className="w-full  m-auto"
+            src={require("../assets/images/book.png")}
+            alt=""
+          />
+        </aside>
 
+        <main className=" col-span-6 lg:col-span-5  p-10 bg-cyan-500">
+          <h1 className=" text-2xl">عنوان الكتاب</h1>
 
-<aside className="  col-span-4 md:col-span-2 md:col-start-3 lg:col-span-1 col-start-2">
-    <img className="w-full  m-auto" src={logo1} alt=""/>
-</aside>
+          <div className="rating text-start flex  mt-5">
+            <div className="star-outer relative me-1 ">
+              <div className="star-inner absolute top-0"></div>
+            </div>
+            <div className="star-outer relative me-1 ">
+              <div className="star-inner absolute top-0"></div>
+            </div>
+            <div className="star-outer relative me-1 ">
+              <div className="star-inner absolute top-0"></div>
+            </div>
+            <div className="star-outer relative me-1 ">
+              <div className="star-inner absolute top-0"></div>
+            </div>
+            <div className="star-outer relative  "></div>
 
-<main className=" col-span-6 lg:col-span-5  p-10 bg-cyan-500">
-    <h1 className=" text-2xl">عنوان الكتاب</h1>
+            <span className="relative ms-5"> 4.0 </span>
+          </div>
 
-    <div className="rating text-start flex  mt-5">
-
-        <div className="star-outer relative me-1 ">
-            <div className="star-inner absolute top-0"></div>
-        </div>
-        <div className="star-outer relative me-1 ">
-            <div className="star-inner absolute top-0"></div>
-        </div>
-        <div className="star-outer relative me-1 ">
-            <div className="star-inner absolute top-0"></div>
-        </div>
-        <div className="star-outer relative me-1 ">
-            <div className="star-inner absolute top-0"></div>
-        </div>
-        <div className="star-outer relative  ">
-
-        </div>
-
-        <span className="relative ms-5"> 4.0 </span>
-    </div>
-
-    <p className="pt-2">﴿یَوۡمَ یَقُولُ ٱلۡمُنَـٰفِقُونَ وَٱلۡمُنَـٰفِقَـٰتُ لِلَّذِینَ ءَامَنُوا۟ ٱنظُرُونَا
-        نَقۡتَبِسۡ مِن نُّورِكُمۡ قِیلَ ٱرۡجِعُوا۟ وَرَاۤءَكُمۡ فَٱلۡتَمِسُوا۟ نُورࣰاۖ فَضُرِبَ بَیۡنَهُم
-        بِسُورࣲ لَّهُۥ بَابُۢ بَاطِنُهُۥ فِیهِ ٱلرَّحۡمَةُ وَظَـٰهِرُهُۥ مِن قِبَلِهِ ٱلۡعَذَابُ ۝١٣
-        یُنَادُونَهُمۡ أَلَمۡ نَكُن مَّعَكُمۡۖ قَالُوا۟ بَلَىٰ وَلَـٰكِنَّكُمۡ فَتَنتُمۡ أَنفُسَكُمۡ
-        وَتَرَبَّصۡتُمۡ وَٱرۡتَبۡتُمۡ وَغَرَّتۡكُمُ ٱلۡأَمَانِیُّ حَتَّىٰ جَاۤءَ أَمۡرُ ٱللَّهِ وَغَرَّكُم
-        بِٱللَّهِ
-
-        <br/>
-
-        ﴿۞ إِنَّ رَبَّكَ یَعۡلَمُ أَنَّكَ تَقُومُ أَدۡنَىٰ مِن ثُلُثَیِ ٱلَّیۡلِ وَنِصۡفَهُۥ وَثُلُثَهُۥ
-        وَطَاۤىِٕفَةࣱ مِّنَ ٱلَّذِینَ مَعَكَۚ وَٱللَّهُ یُقَدِّرُ ٱلَّیۡلَ وَٱلنَّهَارَۚ عَلِمَ أَن لَّن
-        تُحۡصُوهُ فَتَابَ عَلَیۡكُمۡۖ فَٱقۡرَءُوا۟ مَا تَیَسَّرَ مِنَ ٱلۡقُرۡءَانِۚ عَلِمَ أَن سَیَكُونُ مِنكُم
-        مَّرۡضَىٰ وَءَاخَرُونَ یَضۡرِبُونَ فِی ٱلۡأَرۡضِ یَبۡتَغُونَ مِن فَضۡلِ ٱللَّهِ وَءَاخَرُونَ
-        یُقَـٰتِلُونَ فِی سَبِیلِ ٱللَّهِۖ فَٱقۡرَءُوا۟ مَا تَیَسَّرَ مِنۡهُۚ وَأَقِیمُوا۟ ٱلصَّلَوٰةَ
-        وَءَاتُوا۟ ٱلزَّكَوٰةَ وَأَقۡرِضُوا۟ ٱللَّهَ قَرۡضًا حَسَنࣰاۚ وَمَا تُقَدِّمُوا۟ لِأَنفُسِكُم مِّنۡ
-        خَیۡرࣲ تَجِدُوهُ عِندَ ٱللَّهِ هُوَ خَیۡرࣰا وَأَعۡظَمَ أَجۡرࣰاۚ وَٱسۡتَغۡفِرُوا۟ ٱللَّهَۖ إِنَّ ٱللَّهَ
-        غَفُورࣱ رَّحِیمُۢ﴾
-
-
-    </p>
-</main>
-
-
-
-</div>
-
-  </>;
+          <p className="pt-2">
+            ﴿یَوۡمَ یَقُولُ ٱلۡمُنَـٰفِقُونَ وَٱلۡمُنَـٰفِقَـٰتُ لِلَّذِینَ
+            ءَامَنُوا۟ ٱنظُرُونَا نَقۡتَبِسۡ مِن نُّورِكُمۡ قِیلَ ٱرۡجِعُوا۟
+            وَرَاۤءَكُمۡ فَٱلۡتَمِسُوا۟ نُورࣰاۖ فَضُرِبَ بَیۡنَهُم بِسُورࣲ
+            لَّهُۥ بَابُۢ بَاطِنُهُۥ فِیهِ ٱلرَّحۡمَةُ وَظَـٰهِرُهُۥ مِن
+            قِبَلِهِ ٱلۡعَذَابُ ۝١٣ یُنَادُونَهُمۡ أَلَمۡ نَكُن مَّعَكُمۡۖ
+            قَالُوا۟ بَلَىٰ وَلَـٰكِنَّكُمۡ فَتَنتُمۡ أَنفُسَكُمۡ
+            وَتَرَبَّصۡتُمۡ وَٱرۡتَبۡتُمۡ وَغَرَّتۡكُمُ ٱلۡأَمَانِیُّ حَتَّىٰ
+            جَاۤءَ أَمۡرُ ٱللَّهِ وَغَرَّكُم بِٱللَّهِ
+            <br />
+            ﴿۞ إِنَّ رَبَّكَ یَعۡلَمُ أَنَّكَ تَقُومُ أَدۡنَىٰ مِن ثُلُثَیِ
+            ٱلَّیۡلِ وَنِصۡفَهُۥ وَثُلُثَهُۥ وَطَاۤىِٕفَةࣱ مِّنَ ٱلَّذِینَ
+            مَعَكَۚ وَٱللَّهُ یُقَدِّرُ ٱلَّیۡلَ وَٱلنَّهَارَۚ عَلِمَ أَن لَّن
+            تُحۡصُوهُ فَتَابَ عَلَیۡكُمۡۖ فَٱقۡرَءُوا۟ مَا تَیَسَّرَ مِنَ
+            ٱلۡقُرۡءَانِۚ عَلِمَ أَن سَیَكُونُ مِنكُم مَّرۡضَىٰ وَءَاخَرُونَ
+            یَضۡرِبُونَ فِی ٱلۡأَرۡضِ یَبۡتَغُونَ مِن فَضۡلِ ٱللَّهِ
+            وَءَاخَرُونَ یُقَـٰتِلُونَ فِی سَبِیلِ ٱللَّهِۖ فَٱقۡرَءُوا۟ مَا
+            تَیَسَّرَ مِنۡهُۚ وَأَقِیمُوا۟ ٱلصَّلَوٰةَ وَءَاتُوا۟ ٱلزَّكَوٰةَ
+            وَأَقۡرِضُوا۟ ٱللَّهَ قَرۡضًا حَسَنࣰاۚ وَمَا تُقَدِّمُوا۟
+            لِأَنفُسِكُم مِّنۡ خَیۡرࣲ تَجِدُوهُ عِندَ ٱللَّهِ هُوَ خَیۡرࣰا
+            وَأَعۡظَمَ أَجۡرࣰاۚ وَٱسۡتَغۡفِرُوا۟ ٱللَّهَۖ إِنَّ ٱللَّهَ غَفُورࣱ
+            رَّحِیمُۢ﴾
+          </p>
+        </main>
+      </div>
+    </>
+  );
 }
